25c8ab5088a3b209fc04d27ce8cfe997bd465b98
HerbertSousa <herberti1405@outlook.com>
2021-02-27 10:08:48 -0300

    feature: connect api to register user

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 import {Form} from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -40,14 +41,19 @@ const SignUp: React.FC = () => {
 
         await schema.validate(data, { abortEarly: false });
 
-        // await api.post('/users', data);
+        await api.post('/users', data);
 
-        // history.push('/');
+        Alert.alert(
+        'Cadastro realizado com sucesso',
+        'Você ja pode fazer login na aplicação',
+        );
+
+        navigation.navigate('SignIn');
       } catch (err) {
         const errors = getValidationErrors(err);
         formRef.current?.setErrors(errors);
+        Alert.alert('Erro no cadaastro', 'Ocorreu um erro ao fazer o cadastro, tente novamente.');
       }
-      Alert.alert('Erro no cadaastro', 'Ocorreu um erro ao fazer o cadastro, tente novamente.');
     },
     [],
   );
